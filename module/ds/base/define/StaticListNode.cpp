c2145fae47302eac8c0ec17ab63122f22dfc06aa
xiang.li <xiang.li@fa-software.com>
2025-09-07 01:39:46 +0800

    :beers: [2025-09-07-03] - add StaticListNode.cpp

---
+++ module/ds/base/define/StaticListNode.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 一可爱小白兔 on 2025-08-29 10:25.
+//
+
+/**
+ * Define the static list node.
+ */
+struct staticListNode {
+    int data;               // 数据域，存储节点的实际数据
+    int nextIndex;          // 游标，指示下一个节点在数组中的索引，-1表示无后继节点
+};

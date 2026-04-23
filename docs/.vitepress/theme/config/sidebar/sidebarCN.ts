489b1e9b9d0f8d5fbff52cbadb1c12811090debc
yang <yang_000131@163.com>
2026-04-23 23:37:21 +0800

    # transport_layer

--- docs/.vitepress/theme/config/sidebar/sidebarCN.ts
+++ docs/.vitepress/theme/config/sidebar/sidebarCN.ts
@@ -1,3 +1,22 @@
 import { DefaultTheme } from "vitepress";
 
-export const sidebarCN: DefaultTheme.SidebarItem[] = [];
+export const sidebarCN: DefaultTheme.SidebarItem[] = [
+    {
+        text: "计算机网络",
+        items: [
+            {
+                text: "传输层",
+                items: [
+                    {
+                        text: "TCP",
+                        link: "/transport_layer/tcp"
+                    },
+                    {
+                        text: "UDP",
+                        link: "/transport_layer/udp"
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ docs/cn/transport_layer/udp.md
@@ -0,0 +1,25 @@
+---
+author: 一可爱小白兔
+firstCommit: 2026-04-23
+version: 1.0.0
+---
+
+# UDP
+
+## UDP 首部
+
+![UDP 首部](../../resource/image/cn/udp_首部.png){max-width=600px}
+
+UDP 首部的<span class="marker-text marker-red">**长度固定**为 8 个字节</span>
+
+- **源端口**：2 个字节
+- **目标端口**：2 个字节
+- **数据长度**：2 个字节
+- **校验和**：2 个字节
+
+## UDP 校验
+
+## UDP 特点
+
+- **无连接性**
+- **不可靠性**
